9007bf1dae80cf118933fb1fa47ce42398407de6
Joao Mesquita <joaoalmeida48@outlook.com>
2023-02-18 21:58:32 -0300

    commit inicial Projeto

---
+++ ExerciciosTryCatch/Entities/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExerciciosTryCatch.Exceptions;
+
+namespace ExerciciosTryCatch.Entities
+{
+    internal class Account
+    {
+        public int NumberAccount { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; }
+
+        // constructor default
+        public Account(int numberAccount , string holder, double balance, double withDrawLimit ) 
+        {
+            NumberAccount = numberAccount;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        // method to deposit
+        public void Deposit(double Amount)
+        {
+            Balance += Amount;
+        }
+
+        // method to WithDraw
+        public void WithDraw(double Amount)
+        {
+            // if amount bigger then Balance
+            if (Amount > Balance)
+            {
+                Console.WriteLine(new DomainException("Withdraw error: Not enough balance"));
+            }
+            // if Amount bigger then WithDrawLimit
+            else if (Amount > WithDrawLimit) 
+            {
+                Console.WriteLine(new DomainException("Withdraw error: The amount exceeds withdraw limit"));
+            }
+            else // if amount it's Ok do this else
+            {
+                Balance -= Amount;
+                Console.WriteLine("New balance: " + Balance.ToString());
+            }
+        }
+    }
+}
---
+++ ExerciciosTryCatch/Program.cs
@@ -0,0 +1,34 @@
+﻿using ExerciciosTryCatch.Entities;
+using ExerciciosTryCatch.Exceptions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+        Console.WriteLine("Enter account data");
+        Console.Write("Number: ");
+        int numberAccount = int.Parse(Console.ReadLine());
+
+        Console.Write("Holder Account: ");
+        string holder = Console.ReadLine();
+
+        Console.Write("Initial balance: ");
+        double balanceInitial = double.Parse(Console.ReadLine());
+
+        Console.Write("Withdraw limit: ");
+        double withDrawLimit = double.Parse(Console.ReadLine());
+
+        // instantiate a new class Account with parametres
+        Account Account = new Account(numberAccount, holder, balanceInitial, withDrawLimit);
+
+        Console.Write("Enter amount for withdraw: ");
+        double Amount = double.Parse(Console.ReadLine());
+
+        Account.WithDraw(Amount);
+
+
+
+    }
+}
